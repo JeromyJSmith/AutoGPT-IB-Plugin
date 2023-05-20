13edad8f5d698d062f6ec810708e0d982656ca21
hurrisonma <130815017+hurrisonma@users.noreply.github.com>
2023-05-20 16:48:09 +0800

    1

--- autogpt_ib_plugin/ib_plugin.py
+++ autogpt_ib_plugin/ib_plugin.py
@@ -1,4 +1,5 @@
 from ib_insync import *
+import pandas as pd
 
 class IBPlugin:
     def __init__(self):
@@ -10,3 +11,24 @@ class IBPlugin:
         self.ib.qualifyContracts(contract)
         [ticker] = self.ib.reqTickers(contract)
         return ticker.marketPrice()
+
+    def get_historical_data(self, symbol, durationStr='1 Y', barSizeSetting='1 day'):
+        contract = Stock(symbol, 'SMART', 'USD')
+        self.ib.qualifyContracts(contract)
+        bars = self.ib.reqHistoricalData(
+            contract,
+            endDateTime='',
+            durationStr=durationStr,
+            barSizeSetting=barSizeSetting,
+            whatToShow='TRADES',
+            useRTH=True,
+            formatDate=1
+        )
+        return util.df(bars)
+
+    def place_order(self, symbol, action, quantity):
+        contract = Stock(symbol, 'SMART', 'USD')
+        self.ib.qualifyContracts(contract)
+        order = MarketOrder(action, quantity)
+        trade = self.ib.placeOrder(contract, order)
+        return trade
